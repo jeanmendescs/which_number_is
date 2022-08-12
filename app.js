d5989bfe2bdf18972d4be4808026dc70ca861b56
jeanmendescs <mendes.jean.cs@gmail.com>
2022-08-11 21:20:19 -0300

    add  onFetchHandle

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 let currentGuess;
-const userGuess = document.getElementById("user-guess");
-const input = document.getElementById("input");
 
-const updateValue = (string) => {
-  currentGuess = string;
-  userGuess.innerHTML = string;
-  input.value = string;
+const userGuess = document.getElementById("user-guess");
+const statusGuess = document.getElementById("status-guess");
+const input = document.getElementById("input");
+const sendButton = document.getElementById("send-button");
+const newMatchButton = document.getElementById("new-match");
+
+const updateValue = (value) => {
+  currentGuess = Number(value);
+  input.value = value;
 };
 
 const onInputHandle = (value) => {
@@ -20,14 +23,49 @@ const onInputHandle = (value) => {
   updateValue(onlyNumbersCheck.join(""));
 };
 
+const onFecthDataHandle = (value, isError = false) => {
+  console.log(currentGuess, value);
+  const hasWon = currentGuess === value;
+  const isLesser = currentGuess < value;
+
+  input.value = "";
+  userGuess.innerHTML = currentGuess;
+  statusGuess.classList.remove("d-none");
+
+  if (!hasWon && !isError) {
+    statusGuess.innerHTML = isLesser ? "É menor" : "É maior";
+    input.disabled = false;
+    currentGuess = "";
+    return;
+  }
+
+  statusGuess.innerHTML = hasWon ? "Você acertou!" : "Erro";
+  newMatchButton.classList.remove("d-none");
+  input.disabled = true;
+  sendButton.disabled = true;
+};
+
+const onNewMatchHandle = () => {
+  updateValue("");
+  userGuess.innerHTML = "";
+  statusGuess.innerHTML = 0;
+  statusGuess.classList.add("d-none");
+  newMatchButton.classList.add("d-none");
+  input.disabled = false;
+  sendButton.disabled = false;
+};
+
 const fetchData = () => {
-  fetch("https://us-central1-ss-devops.cloudfunctions.net/rand?min=1&max=300")
+  fetch("https://us-central1-ss-devops.cloudfunctions.net/rand?min=1&max=2")
     .then((data) => data.json())
-    .then((data) => console.log(data))
-    .catch((err) => {
-      console.log(err);
-      console.log("erro");
-    });
+    .then((data) => {
+      if (Object.keys(data).length > 1) {
+        onFecthDataHandle(Number(data.StatusCode), true);
+        return;
+      }
+      onFecthDataHandle(Number(data.value));
+    })
+    .catch((err) => console.error(err));
 };
 
 const onClickHandle = () => {
